938ec427a7568119de2f558ac5b5ef4389342101
Nathan Simpson <hello@nathansimpson.design>
2019-06-08 15:11:56 +1000

    GitHub!

---
+++ packages/services/github/index.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+const accessToken = process.env.GITHUB_ACCESS_TOKEN;
+
+exports.getProfile = async () => {
+  const result = await axios({
+    url: "https://api.github.com/graphql",
+    method: "post",
+    headers: { Authorization: `bearer ${accessToken}` },
+    data: {
+      query: `
+        query {
+          viewer {
+            login
+            name
+            bio
+            company
+            email
+            id
+            location
+            repositories(first: 100, privacy:PUBLIC){
+              nodes {
+                id
+                name
+                url
+              }
+            }
+          }
+        }
+        `
+    }
+  }).catch(err => {
+    throw new Error(err.response.statusText);
+  });
+
+  return result.data;
+};
